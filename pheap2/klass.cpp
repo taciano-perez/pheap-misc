b31c82f5cbdde9e7202eaac2523d552c21f64e0c
Taciano Perez <taciano.perez@hp.com>
2016-02-15 23:22:48 +0000

    allocated a graph of classes using a custom allocator on top of NVML

--- pheap2/klass.cpp
+++ pheap2/klass.cpp
@@ -1,9 +1,26 @@
 #include "klass.hpp"
+#include <libpmemobj.h>
 
 char* Klass::get_class_name() {
 	return class_name;
 }
 
-void Klass::set_class_name(const char* klass_name) {
-	class_name = klass_name;
+void Klass::set_class_name(char* klass_name) {
+	TX_BEGIN(get_pop()) {
+		pmemobj_tx_add_range_direct(&class_name, sizeof(char*));
+		class_name = klass_name;
+		//pmemobj_persist(get_pop(), &class_name, sizeof (char*));
+	} TX_END
+}
+
+Symbol* Klass::get_symbol() {
+		return symbol;
+}
+
+void Klass::set_symbol(Symbol* s) {
+	TX_BEGIN(get_pop()) {
+		pmemobj_tx_add_range_direct(&symbol, sizeof(Symbol*));
+		symbol = s;
+		//pmemobj_persist(get_pop(), &class_name, sizeof (char*));
+	} TX_END
 }
--- pheap2/klass.hpp
+++ pheap2/klass.hpp
@@ -4,22 +4,27 @@
 #include <libpmemobj.h>
 
 #include "pop.h"
+#include "symbol.h"
 
 class Klass {
 
 	private:
 	char* class_name;
+	Symbol* symbol;
 
 	public:
 	void *operator new(size_t  num_bytes) {
 		void* result;
-		TX_BEGIN(get_pop()) {
-			result = pmemobj_direct(pmemobj_tx_alloc(num_bytes, 0));
-		} TX_END
+		// TX_BEGIN(get_pop()) {
+			// result = pmemobj_direct(pmemobj_tx_alloc(num_bytes, 0));
+		// } TX_END
+		result = ph_malloc(num_bytes);
 		return result;
 	}
 	char* get_class_name();
-	void set_class_name(const char* klass_name);
+	void set_class_name(char* klass_name);
+	Symbol* get_symbol();
+	void set_symbol(Symbol* s);
 
 };
 
